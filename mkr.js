e8b76b4c4410046befbd1f8eb01da8f83e54a484
Mohammad Chughtai <mohammadhunan@gmail.com>
2017-03-12 14:54:54 -0400

    Added random num, except function with optional 3p

--- mkr.js
+++ mkr.js
@@ -17,3 +17,18 @@ doc.getByClass = function(element,n){
 }
 
 
+function getRand(max,min,except){   //inclusive inclusive
+  
+  min = Math.round(min);
+  max = Math.round(max)+1;
+  let tempmax = max-1;
+  let tempmin = min;
+  let tempexc = except;
+  let randNum = Math.floor(Math.random() * (max - min)) + min;
+  if(randNum === except){
+      return getRand(tempmax,tempmin,tempexc);
+  }else{    //works in all cases even if except is undefined
+      return randNum;
+  }
+
+}
